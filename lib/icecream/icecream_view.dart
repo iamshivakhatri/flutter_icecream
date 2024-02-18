8c980bdc09d50e23329d2f4cba50b213fe996cfe
Shiva Khatri <shivakhatri@Shivas-Air.lan>
2024-02-18 16:22:03 -0500

    Changes on UI

--- lib/icecream/icecream_view.dart
+++ lib/icecream/icecream_view.dart
@@ -31,29 +31,80 @@ class IcecreamView extends StatelessWidget {
             style: Theme.of(context).textTheme.bodySmall!,
           ),
           Expanded(
-            child: FutureBuilder<List<Icecream>?>(
-              future: loadIcecreams(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  final icecreams = snapshot.data!;
-                  return ListView.builder(
-                    itemCount: icecreams.length,
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      final icecream = icecreams[index];
-                      return Card(
-                        child: Text(icecream.flavor),
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator.adaptive(),
-                  );
-                }
-              },
-            ),
+            child: Column(
+              children:[ 
+                FutureBuilder<List<Icecream>?>(
+                future: loadIcecreams(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    final icecreams = snapshot.data!;
+                    return SizedBox(
+                      width: MediaQuery.of(context).size.width, 
+                      height: MediaQuery.of(context).size.height/2.5,
+                      child: ListView.builder(
+                        itemCount: icecreams.length,
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          final icecream = icecreams[index];
+                          return SizedBox(
+                            width: 200,
+                            height: 100,
+                            child: Card(
+                              clipBehavior: Clip.hardEdge,
+                              color: Colors.yellow[100],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Stack(
+                                fit: StackFit.expand,
+                                children: [
+                                  Image.network(
+                                    icecream.image!,
+                                    fit: BoxFit.cover,
+                                    height: 400,
+                                    ),
+                                  Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                      
+                                        children: [
+                                          Text(
+                                        icecream.flavor,
+                                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "\$${icecream.price.toString()}",
+                                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                        ],)
+                                    )
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  }
+                },
+              ),
+        ]),
           ),
         ],
       ),
